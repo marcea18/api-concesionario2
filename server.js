aa37cdd7550ec9d835222581dc99fefe04f607f0
marcea18 <marcea18@gmail.com>
2021-10-30 11:40:35 -0500

    Agregar el Back

---
+++ server.js
@@ -0,0 +1,117 @@
+// hacer el import de express tradicional
+// const express = require('express');
+
+// hacer el nuevo import
+import Express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import Cors from 'cors';
+
+const stringConexion = 
+'mongodb+srv://marcea:Admin2021@proyectoconcesionario.z6j8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const client = new MongoClient(stringConexion, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let baseDeDatos;
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Cors());
+
+app.get('/vehiculos', (req, res) => {
+  console.log('alguien hizo get en la ruta /vehiculos');
+  baseDeDatos
+  .collection("vehiculo")
+  .find({})
+  .limit(50)
+  .toArray((err, result)=>{
+    if (err){
+        res.status(500).send("Error consultando los vehículos");
+    } else {
+        res.json(result);
+    }
+  });
+});
+
+app.post('/vehiculos/nuevo', (req, res) => {
+  console.log(req);
+  const datosVehiculo = req.body;
+  console.log('llaves: ', Object.keys(datosVehiculo));
+  try {
+    if (
+      Object.keys(datosVehiculo).includes('name') &&
+      Object.keys(datosVehiculo).includes('brand') &&
+      Object.keys(datosVehiculo).includes('model')
+    ) {
+      // implementar código para crear vehículo en la BD
+      baseDeDatos.collection('vehiculo').insertOne(datosVehiculo,(err, result)=>{
+          if (err) {
+              console.error(err);
+              res.sendStatus(500);
+          } else { 
+              console.log(result);
+              res.sendStatus(200);
+          }
+      });
+    } else {
+      res.sendStatus(500);
+    }
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+app.patch('/vehiculos/editar', (req, res) => {
+  const edicion = req.body;
+  console.log(edicion);
+  const filtroVehiculo = {_id: new ObjectId(edicion.id) };
+  delete edicion.id;  
+  const operacion = {
+    $set: edicion,
+  };
+  baseDeDatos
+  .collection('vehiculo')
+  .findOneAndUpdate(filtroVehiculo, 
+    operacion, 
+    {upsert: true, returnOriginal: true}, 
+    (err, result) => {
+      if(err){
+        console.error("Error actualizando el vehículo", err);
+        res.sendStatus(500);
+      } else {
+      console.log("Actualizado con exito");
+      res.sendStatus(200);
+    }
+  });
+});
+
+app.delete('/vehiculos/eliminar', (req, res) => {
+  const filtroVehiculo = {_id: new ObjectId(req.body.id) };
+  baseDeDatos.collection('vehiculo').deleteOne(filtroVehiculo, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+const main = () => {
+
+    client.connect((err,db)=>{
+        if(err){
+            console.error("Error conectando a la BD");
+        }
+        baseDeDatos = db.db('concesionario');
+        console.log('Conexión a BD exitosa');
+        return app.listen(5000, () => {
+        console.log('escuchando puerto 5000');
+      });
+    });
+};
+
+main();
